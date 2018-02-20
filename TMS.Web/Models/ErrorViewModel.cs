4d91c6d146be6181bc56ed83a2713c1b72afd00a
vijay <vijay@sulabs.tech>
2018-02-20 14:08:51 +0530

    my first tms commit

--- TMS.Web/Models/ErrorViewModel.cs
+++ TMS.Web/Models/ErrorViewModel.cs
@@ -11,6 +11,7 @@ namespace TMS.Web.Models
 {
     public class ErrorViewModel
     {
+
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
